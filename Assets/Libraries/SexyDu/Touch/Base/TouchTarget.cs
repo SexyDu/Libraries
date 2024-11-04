b6e89c3ed470e5633737dcf027dfd11d3a88831a
SexyDu <128912129+SexyDu@users.noreply.github.com>
2024-11-05 01:42:41 +0900

    멀티터치 이동 베이스 완료

--- Assets/Libraries/SexyDu/Touch/Base/TouchTarget.cs
+++ Assets/Libraries/SexyDu/Touch/Base/TouchTarget.cs
@@ -14,6 +14,12 @@ namespace SexyDu.Touch
 
         protected TouchConfig Config => TouchCenter.Config;
 
+        /// <summary>
+        /// Unity Position Per One Pixel
+        /// TouchCenter가 송출하는 화면의 1 픽셀 당 유니티 위치(크기) 값
+        /// </summary>
+        protected float UPPOP => Config.WorldPosPerOnePixel;
+
         protected Vector2 GetTouchPosition(int fingerId)
         {
             return Config.GetTouchPosition(fingerId);
--- Assets/Libraries/SexyDu/Touch/Childs/MultiTouchBase.cs
+++ Assets/Libraries/SexyDu/Touch/Childs/MultiTouchBase.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace SexyDu.Touch
 {
-    public abstract class MultiTouchBase : TouchTarget
+    public /*abstract*/ class MultiTouchBase : TouchTarget
     {
         public override void AddTouch(int fingerId)
         {
             if (touches.Count < MaxTouchCount)
             {
                 touches.Add(fingerId);
+
+                if (!IsRunning)
+                    RunRoutine();
             }
         }
 
@@ -19,12 +23,45 @@ namespace SexyDu.Touch
             touches.Remove(fingerId);
         }
 
+        protected virtual void RemoveTouches(params int[] fingerIds)
+        {
+            for (int i = 0; i < fingerIds.Length; i++)
+            {
+                touches.Remove(fingerIds[i]);
+            }
+
+            if (touches.Count == 0)
+            {
+                StopRoutine();
+
+                data.Clear();
+                previous = Vector2.zero;
+            }
+        }
+
         public override void ClearTouch()
         {
             touches.Clear();
         }
 
-        #region Touch
+        #region Transform
+        [SerializeField] private Transform target;
+        private Vector3 position = Vector2.zero;
+
+        protected virtual void Translate(Vector2 delta)
+        {
+            SetPosition(position.x + delta.x, position.y + delta.y);
+        }
+
+        protected virtual void SetPosition(float x, float y)
+        {
+            position.x = x;
+            position.y = y;
+            target.position = position;
+        }
+        #endregion
+
+        #region TouchData
         protected List<int> touches = new List<int>(10);
 
         public int Count => touches.Count;
@@ -39,40 +76,190 @@ namespace SexyDu.Touch
                 return GetTouchPosition(touches[index]);
         }
 
-        protected RealtimeMultiTouch GetMultiTouch()
+        protected virtual TouchData[] GetTouchDatas()
         {
-            RealtimeTouch[] realtimeTouches = new RealtimeTouch[Count];
-            for (int i = 0; i < realtimeTouches.Length; i++)
+            TouchData[] datas = new TouchData[touches.Count];
+            for (int i = 0; i < datas.Length; i++)
             {
-                realtimeTouches[i] = new RealtimeTouch(touches[i]);
+                datas[i] = new TouchData(touches[i]);
             }
-            return new RealtimeMultiTouch(realtimeTouches);
+
+            return datas;
+        }
+        #endregion
+
+        #region TouchRoutine
+        private Vector2 previous = Vector2.zero;
+        private MultiTouchData data = new MultiTouchData();
+
+        private IEnumerator ieRoutine = null;
+
+        private bool IsRunning => ieRoutine != null;
+
+        private IEnumerator CoRoutine()
+        {
+            do
+            {
+                if (data.Count == touches.Count)
+                    ProcessTouch();
+                else
+                    InitializeTouch();
+
+                yield return null;
+
+                Debug.LogFormat("러닝 중");
+            } while (true);
+        }
+
+        private void InitializeTouch()
+        {
+            position = target.position;
+
+            data.Set(touches.Count);
+            data.Set(GetTouchDatas());
+
+            int[] invalidFingers = data.GetInvalidFingerIds();
+            if (invalidFingers != null)
+            {
+                RemoveTouches(invalidFingers);
+            }
+            else
+            {
+                previous = data.Center;
+            }
+        }
+
+        private void ProcessTouch()
+        {
+            data.Set(GetTouchDatas());
+            int[] invalidFingers = data.GetInvalidFingerIds();
+            if (invalidFingers != null)
+            {
+                RemoveTouches(invalidFingers);
+            }
+            else
+            {
+                Vector2 current = data.Center;
+
+                Vector2 delta = Vector2.zero;
+
+                delta += (current - previous) * UPPOP;
+
+                Translate(delta);
+
+                previous = data.Center;
+            }
+        }
+
+        private void RunRoutine()
+        {
+            StopRoutine();
+
+            ieRoutine = CoRoutine();
+            StartCoroutine(ieRoutine);
+        }
+
+        private void StopRoutine()
+        {
+            if (ieRoutine != null)
+            {
+                StopCoroutine(ieRoutine);
+                ieRoutine = null;
+            }
         }
         #endregion
     }
 
-    public struct RealtimeMultiTouch
+    public struct MultiTouchData
     {
-        public RealtimeMultiTouch(params RealtimeTouch[] touches)
-        {
+        private TouchData[] touches;
+        private int count;
+        private float countForMult;
 
+        public int Count => count;
+
+        public bool IsValid
+        {
+            get
+            {
+                for (int i = 0; i < touches.Length; i++)
+                {
+                    if (!touches[i].IsValid)
+                        return false;
+                }
+
+                return false;
+            }
+        }
+
+        public void Set(int count)
+        {
+            this.count = count;
+            countForMult = 1f / (float)count;
+        }
+
+        public void Set(TouchData[] touches)
+        {
+            this.touches = touches;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+            countForMult = 0f;
+        }
+
+        public Vector2 Center
+        {
+            get
+            {
+                Vector2 sum = Vector2.zero;
+
+                for (int i = 0; i < touches.Length; i++)
+                {
+                    sum += touches[i].position;
+                }
+
+                return sum *= countForMult;
+            }
+        }
+
+        public int[] GetInvalidFingerIds()
+        {
+            List<int> fingerIds = null;
+
+            for (int i = 0; i < touches.Length; i++)
+            {
+                if (!touches[i].IsValid)
+                {
+                    if (fingerIds == null)
+                        fingerIds = new List<int>(touches[i].fingerId);
+                    
+                    fingerIds.Add(touches[i].fingerId);
+                }
+            }
+
+            if (fingerIds == null)
+                return null;
+            else
+                return fingerIds.ToArray();
         }
     }
 
-    public struct RealtimeTouch
+    public struct TouchData
     {
         public readonly int fingerId;
         public readonly Vector2 position;
 
         public bool IsValid => TouchCenter.Config.ValidateTouchPosition(position);
 
-        public RealtimeTouch(int fingerId)
+        public TouchData(int fingerId)
         {
             this.fingerId = fingerId;
             position = TouchCenter.Config.GetTouchPosition(this.fingerId);
         }
 
-        public RealtimeTouch(int fingerId, Vector2 position)
+        public TouchData(int fingerId, Vector2 position)
         {
             this.fingerId = fingerId;
             this.position = position;
